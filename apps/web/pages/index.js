92452bc5a85878bf418e778fb910c440f68e1a86
Sam Hindess <mail@sohcah.dev>
2021-07-03 15:04:46 +0100

    Initial Setup

--- apps/web/pages/index.js
+++ apps/web/pages/index.js
@@ -2,79 +2,78 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>CuppaZee</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main className="flex flex-col items-stretch w-full">
+        <section className="bg-[#03C35B] min-h-[600px] flex flex-col lg:flex-row-reverse px-4 py-8 lg:px-20 items-center text-center lg:text-right">
+          <img src="/max.png" className="h-[80%] max-h-[500px] lg:max-w-[40%]" />
+          <div className="flex-grow">
+            <h1 className="text-4xl lg:text-6xl font-bold">CuppaZee Max</h1>
+            <h2 className="text-3xl lg:text-4xl">Feature Packed.</h2>
+            <h3 className="text-3xl"><a href="https://max.cuppazee.app">https://max.cuppazee.app</a></h3>
+          </div>
+          <div className="flex flex-col md:flex-row lg:flex-col align-center">
+            <a href="https://apps.apple.com/us/app/cuppazee-max/id1514563308"><img src="/appstore.svg" className="h-12 md:h-16 m-2" /></a>
+            <a href="https://play.google.com/store/apps/details?id=uk.cuppazee.paper"><img src="/googleplay.svg" className="h-12 md:h-16 m-2" /></a>
+            <a href="https://max.cuppazee.app/"><img src="/pwa.png" className="h-12 md:h-16 m-2" /></a>
+          </div>
+        </section>
+        <section className="bg-[#43BCCA] min-h-[600px] flex flex-col lg:flex-row px-4 py-8 lg:px-20 items-center text-center lg:text-left">
+          <img src="/express.png" className="h-[80%] max-h-[500px]" />
+          <div className="flex-grow">
+            <h1 className="text-4xl lg:text-6xl font-bold">CuppaZee Express</h1>
+            <h2 className="text-3xl lg:text-4xl">Simple. Fast. Effective.</h2>
+            <h3 className="text-3xl">Beta Testing: <a href="https://facebook.com/groups/cuppazee">https://facebook.com/groups/cuppazee</a></h3>
+            {/* <h3 className="text-3xl"><a href="https://express.cuppazee.app">https://express.cuppazee.app</a></h3> */}
+          </div>
+          {/* <div className="flex flex-col md:flex-row lg:flex-col align-center">
+            <a href="https://apps.apple.com/us/app/cuppazee-express/id1570474744"><img src="/appstore.svg" className="h-12 md:h-16 m-2" /></a>
+            <a href="https://play.google.com/store/apps/details?id=app.cuppazee.express"><img src="/googleplay.svg" className="h-12 md:h-16 m-2" /></a>
+            <a href="https://express.cuppazee.app/"><img src="/pwa.png" className="h-12 md:h-16 m-2" /></a>
+          </div> */}
+        </section>
+        <section className="bg-[#af4eff] lg:min-h-[300px] flex flex-col lg:flex-row px-4 py-8 lg:px-20 items-center text-center lg:text-left">
+          {/* <img src="/max.png" className="h-[80%] max-h-[500px] lg:max-w-[40%]" /> */}
+          <div className="flex-grow">
+            <h1 className="text-4xl lg:text-6xl font-bold">CuppaZee Painter</h1>
+            <h2 className="text-3xl lg:text-4xl">Design. Publish. Deploy.</h2>
+            <h3 className="text-3xl">Coming Soon</h3>
+            {/* <h3 className="text-3xl"><a href="https://max.cuppazee.app">https://max.cuppazee.app</a></h3> */}
+          </div>
+          {/* <div>
+            <a href=""><img src="/appstore.svg" className="h-16 my-2" /></a>
+            <a href="https://play.google.com/store/apps/details?id=uk.cuppazee.paper"><img src="/googleplay.svg" className="h-16 my-2" /></a>
+            <a href="https://max.cuppazee.app/"><img src="/pwa.png" className="h-16 my-2" /></a>
+          </div> */}
+        </section>
+        {/* <section className="bg-[#ffd95c] lg:min-h-[300px] flex flex-col lg:flex-row px-4 py-8 lg:px-20 items-center text-center lg:text-left"> */}
+          {/* <img src="/max.png" className="h-[80%] max-h-[500px] lg:max-w-[40%]" /> */}
+          {/* <div className="flex-grow">
+            <h1 className="text-4xl lg:text-6xl font-bold">CuppaZee Extension</h1>
+            <h2 className="text-3xl lg:text-4xl">Better Munzee Map.</h2>
+            <h3 className="text-3xl">Coming Soon</h3> */}
+            {/* <h3 className="text-3xl"><a href="https://max.cuppazee.app">https://max.cuppazee.app</a></h3> */}
+          {/* </div> */}
+          {/* <div>
+            <a href=""><img src="/appstore.svg" className="h-16 my-2" /></a>
+            <a href="https://play.google.com/store/apps/details?id=uk.cuppazee.paper"><img src="/googleplay.svg" className="h-16 my-2" /></a>
+            <a href="https://max.cuppazee.app/"><img src="/pwa.png" className="h-16 my-2" /></a>
+          </div> */}
+        {/* </section> */}
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
+      <footer className="bg-black text-white flex flex-row justify-center w-full h-24">
         <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          className="flex flex-grow items-center justify-center"
+          href="https://sohcah.dev"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+          Developed by Sam Hindess
         </a>
       </footer>
     </div>
