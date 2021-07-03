96c56a8435d2096373b5b995585edda3f0f4a3bf
Sam Hindess <mail@sohcah.dev>
2021-07-03 15:35:36 +0100

    Design Improvements

--- apps/web/pages/index.js
+++ apps/web/pages/index.js
@@ -26,31 +26,31 @@ export default function Home() {
             }} className="bg-[#03C35B] text-black p-2 rounded-md">Migrate your Data</button>
           </div>
         </section>}
-        <section key="max" className="bg-[#03C35B] min-h-[600px] flex flex-col lg:flex-row-reverse px-4 py-8 lg:px-20 items-center text-center lg:text-right">
+        <section key="max" className="bg-[#03C35B] min-h-[400px] lg:min-h-[600px] flex flex-col lg:flex-row-reverse px-4 py-8 lg:px-20 items-center text-center lg:text-right">
           <img src="/max.png" className="max-h-[500px] lg:max-w-[40%]" />
-          <div className="flex-grow">
+          <div className="lg:flex-grow">
             <h1 className="text-4xl lg:text-6xl font-bold">CuppaZee Max</h1>
             <h2 className="text-3xl lg:text-4xl">Feature Packed.</h2>
             <h3 className="text-1xl"><a href="https://max.cuppazee.app">https://max.cuppazee.app</a></h3>
           </div>
-          <div className="flex flex-col md:flex-row lg:flex-col align-center">
-            <a href="https://apps.apple.com/us/app/cuppazee-max/id1514563308"><img src="/appstore.svg" className="h-12 md:h-16 m-2" /></a>
-            <a href="https://play.google.com/store/apps/details?id=uk.cuppazee.paper"><img src="/googleplay.svg" className="h-12 md:h-16 m-2" /></a>
-            <a href="https://max.cuppazee.app/"><img src="/pwa.png" className="h-12 md:h-16 m-2" /></a>
+          <div className="flex flex-row flex-wrap justify-center w-100 lg:w-auto lg:flex-col align-center">
+            <a href="https://apps.apple.com/us/app/cuppazee-max/id1514563308"><img src="/appstore.svg" className="h-12 md:h-16 m-1" /></a>
+            <a href="https://play.google.com/store/apps/details?id=uk.cuppazee.paper"><img src="/googleplay.svg" className="h-12 md:h-16 m-1" /></a>
+            <a href="https://max.cuppazee.app/"><img src="/pwa.png" className="h-12 md:h-16 m-1" /></a>
           </div>
         </section>
-        <section key="express" className="bg-[#43BCCA] min-h-[600px] flex flex-col lg:flex-row px-4 py-8 lg:px-20 items-center text-center lg:text-left">
+        <section key="express" className="bg-[#43BCCA] min-h-[400px] lg:min-h-[600px] flex flex-col lg:flex-row px-4 py-8 lg:px-20 items-center text-center lg:text-left">
           <img src="/express.png" className="max-h-[500px]" />
-          <div className="flex-grow">
+          <div className="lg:flex-grow">
             <h1 className="text-4xl lg:text-6xl font-bold">CuppaZee Express</h1>
             <h2 className="text-3xl lg:text-4xl">Simple. Fast. Effective.</h2>
             <h3 className="text-1xl">Beta Testing: <a href="https://facebook.com/groups/cuppazee">https://facebook.com/groups/cuppazee</a></h3>
             {/* <h3 className="text-3xl"><a href="https://express.cuppazee.app">https://express.cuppazee.app</a></h3> */}
           </div>
-          {/* <div className="flex flex-col md:flex-row lg:flex-col align-center">
-            <a href="https://apps.apple.com/us/app/cuppazee-express/id1570474744"><img src="/appstore.svg" className="h-12 md:h-16 m-2" /></a>
-            <a href="https://play.google.com/store/apps/details?id=app.cuppazee.express"><img src="/googleplay.svg" className="h-12 md:h-16 m-2" /></a>
-            <a href="https://express.cuppazee.app/"><img src="/pwa.png" className="h-12 md:h-16 m-2" /></a>
+          {/* <div className="flex flex-row flex-wrap justify-center w-100 lg:w-auto lg:flex-col align-center">
+            <a href="https://apps.apple.com/us/app/cuppazee-express/id1570474744"><img src="/appstore.svg" className="h-12 md:h-16 m-1" /></a>
+            <a href="https://play.google.com/store/apps/details?id=app.cuppazee.express"><img src="/googleplay.svg" className="h-12 md:h-16 m-1" /></a>
+            <a href="https://express.cuppazee.app/"><img src="/pwa.png" className="h-12 md:h-16 m-1" /></a>
           </div> */}
         </section>
         <section key="painter" className="bg-[#af4eff] lg:min-h-[300px] flex flex-col lg:flex-row px-4 py-8 lg:px-20 items-center text-center lg:text-left">
