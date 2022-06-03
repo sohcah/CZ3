acb534a6d6ab1ba75496150cb21c827bc64d536f
Sam Hindess <mail@sohcah.dev>
2022-06-03 19:34:26 +0000

    Deploy New Alpha App

--- app/next/next.config.js
+++ app/next/next.config.js
@@ -1,57 +1,58 @@
 /** @type {import('next').NextConfig} */
-const withPlugins = require('next-compose-plugins')
-const { withTamagui } = require('@tamagui/next-plugin')
+const withPlugins = require("next-compose-plugins");
+const { withTamagui } = require("@tamagui/next-plugin");
 // const { withExpo } = require('@expo/next-adapter')
-const withTM = require('next-transpile-modules')
+const withTM = require("next-transpile-modules");
+const path = require("path");
 
-process.env.IGNORE_TS_CONFIG_PATHS = 'true'
-process.env.TAMAGUI_TARGET = 'web'
+process.env.IGNORE_TS_CONFIG_PATHS = "true";
+process.env.TAMAGUI_TARGET = "web";
 
-const disableExtraction = process.env.NODE_ENV === 'development'
+const disableExtraction = process.env.NODE_ENV === "development";
 if (disableExtraction) {
-  console.log('Disabling static extraction in development mode for better HMR')
+  console.log("Disabling static extraction in development mode for better HMR");
 }
 
 const transform = withPlugins([
   withTM([
-    'solito',
-    'react-native-web',
-    '@expo/next-adapter',
-    'expo-linking',
-    'expo-constants',
-    'expo-modules-core',
-    '@cz3/app',
-    'react-native-reanimated'
+    "solito",
+    "react-native-web",
+    "@expo/next-adapter",
+    "expo-linking",
+    "expo-constants",
+    "expo-modules-core",
+    "@cz3/app",
+    "react-native-reanimated",
   ]),
   withTamagui({
-    config: './tamagui.config.ts',
-    components: ['@tamagui/core', '@tamagui/drawer', 'tamagui'],
-    importsWhitelist: ['constants.js', 'colors.js'],
+    config: "./tamagui.config.ts",
+    components: ["@tamagui/core", "@tamagui/drawer", "tamagui"],
+    importsWhitelist: ["constants.js", "colors.js"],
     logTimings: true,
     disableExtraction,
-    shouldExtract: (path) => {
-      if (path.includes('app')) {
-        return true
+    shouldExtract: path => {
+      if (path.includes("app")) {
+        return true;
       }
     },
     excludeReactNativeWebExports: [
-      'Switch',
-      'ProgressBar',
-      'Picker',
-      'Animated',
-      'AnimatedFlatList',
-      'VirtualizedList',
-      'VirtualizedSectionList',
-      'FlatList',
-      'CheckBox',
+      "Switch",
+      "ProgressBar",
+      "Picker",
+      "Animated",
+      "AnimatedFlatList",
+      "VirtualizedList",
+      "VirtualizedSectionList",
+      "FlatList",
+      "CheckBox",
     ],
   }),
-])
+]);
 
 module.exports = function (name, { defaultConfig }) {
-  defaultConfig.webpack5 = true
+  defaultConfig.webpack5 = true;
   // defaultConfig.experimental.reactRoot = 'concurrent'
-  defaultConfig.typescript.ignoreBuildErrors = true
+  defaultConfig.typescript.ignoreBuildErrors = true;
   // defaultConfig.pageExtensions = ["web.tsx", "web.ts", "tsx", "ts", "jsx", "js"]
   return transform(name, {
     ...defaultConfig,
@@ -61,6 +62,9 @@ module.exports = function (name, { defaultConfig }) {
       scrollRestoration: true,
       legacyBrowsers: false,
       browsersListForSwc: true,
-    }
-  })
-}
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+  });
+};
