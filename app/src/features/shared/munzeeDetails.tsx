2cf6631479020facc06af5869fd521f1658217e5
Sam Hindess <mail@sohcah.dev>
2022-09-15 22:17:40 +0100

    App: Add Munzee Details Panel to Tourism

--- app/src/features/shared/munzeeDetails.tsx
+++ app/src/features/shared/munzeeDetails.tsx
@@ -1,7 +1,23 @@
-import { useEffect } from "react";
 import { trpc } from "@/common/trpc/trpc";
+import { H2, H3, YStack, Button } from "tamagui";
+import { openBrowserAsync } from "expo-web-browser";
 
 export function MunzeeDetailsPanel({ munzeeId }: { munzeeId: number }) {
   const data = trpc.useQuery(["munzee:details", { munzeeId }]);
-  return null;
+  if (!data.data) return null;
+  return (
+    <YStack>
+      <H2>{data.data.name}</H2>
+      <H3>By {data.data.creator.username}</H3>
+      <Button
+        href={data.data.url}
+        onPress={() => {
+          console.log(data.data.url);
+          openBrowserAsync(data.data.url);
+        }}
+      >
+        Open on Munzee.com
+      </Button>
+    </YStack>
+  );
 }
