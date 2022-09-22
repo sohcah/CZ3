aa1387bab9d2dfe89a15d18325bc46138ff2a1dc
Sam Hindess <mail@sohcah.dev>
2022-09-22 22:29:10 +0100

    API: Automatic tasksByGameId Generation with Cacher

--- api/utils/cacher.ts
+++ api/utils/cacher.ts
@@ -1,17 +1,23 @@
 import fjss from "fast-json-stable-stringify";
+
 export class Cacher<TData, TInput = string | undefined> {
   private cache: Map<string, [Promise<TData>, number]> = new Map();
-  constructor(private func: (input: TInput) => TData | Promise<TData>, private timeout: number) {}
+
+  constructor(
+    private func: (input: TInput) => TData | Promise<TData>,
+    private timeout: number | ((input: TInput) => number)
+  ) {}
 
   async get(...input: TInput extends undefined ? [TInput?] : [TInput]): Promise<TData> {
     const key = fjss(input);
     const cached = this.cache.get(key);
-    if(cached && Date.now() > cached[1]) {
+    if (cached && Date.now() > cached[1]) {
       return await cached[0];
     }
 
     const value = this.func(input[0]!);
-    this.cache.set(key, [Promise.resolve(value), Date.now() + this.timeout]);
+    const timeout = this.timeout instanceof Function ? this.timeout(input[0]!) : this.timeout;
+    this.cache.set(key, [Promise.resolve(value), Date.now() + timeout]);
     return value;
   }
 
--- api/utils/shadow/player.ts
+++ api/utils/shadow/player.ts
@@ -1,10 +1,15 @@
-import { authenticateWithUserID, AuthenticationResult } from "../auth/index.js";
+import {
+  authenticateAnonymous,
+  authenticateWithUserID,
+  AuthenticationResult,
+} from "../auth/index.js";
 import { munzeeFetch } from "../munzee.js";
 import { getShadowPlayerTask } from "./task.js";
 import { ActivityData, addActivityItemExtras } from "./tasks.js";
 import { prisma } from "../prisma.js";
 import { shadow_player, shadow_player_task, shadow_player_task_day } from "@cz3/prisma";
 import { rollbar } from "../../extra/rollbar.js";
+import { Cacher } from "../cacher";
 
 export interface ShadowPlayerReference {
   user_id: number;
@@ -16,17 +21,50 @@ export interface ShadowPlayerReference {
   };
 }
 
-const tasksByGameId: { [game_id: number]: number[] } = {
-  105: [1, 10, 13, 19, 24, 36, 3, 30, 35, 37, 12, 26, 28, 31],
-  106: [1, 2, 6, 12, 24, 28, 30, 35, 36, 37, 5038, 4038, 3038, 2038, 3, 13, 14, 26, 34],
-  107: [1, 2, 6, 13, 19, 24, 35, 10038, 8038, 6038, 5038, 3038, 3, 37, 27, 26, 28, 29, 32, 36],
-  108: [1, 2, 6, 7, 13, 24, 8038, 7038, 5038, 4038, 3038, 3, 17, 30, 35, 37, 26, 28, 33, 36],
-  109: [1, 2, 6, 13, 24, 34, 36, 8038, 6038, 4038, 3038, 3, 35, 37, 12, 14, 26, 28, 30, 31],
-  110: [1, 2, 6, 13, 24, 28, 30, 8038, 6038, 4038, 3038, 3, 27, 35, 37, 7, 19, 26, 29, 36],
-  111: [1, 2, 6, 13, 17, 24, 31, 8038, 6038, 4038, 3038, 3, 35, 37, 14, 26, 28, 30, 36],
-  112: [1, 2, 6, 7, 13, 24, 36, 8038, 6038, 4038, 3038, 3, 35, 37, 26, 28, 30, 32, 34],
-  113: [1, 2, 6, 13, 14, 24, 29, 6038, 5038, 4038, 3038, 2038, 1038, 3, 35, 37, 17, 26, 28, 30, 36],
-};
+function getCuppaZeeTaskId(task: { task_id: number; name: string }) {
+  const n = Number(task.name.match(/with ([0-9]+),000 Points/i)?.[1]);
+  if (Number(task.task_id) === 38) {
+    return n * 1000 + Number(task.task_id);
+  }
+  return Number(task.task_id);
+}
+
+const tasksByGameId = new Cacher(async (game_id: number) => {
+  try {
+    const token = await authenticateAnonymous();
+    const response = await munzeeFetch({
+      endpoint: "clan/v2/requirements",
+      params: {
+        clan_id: 2041,
+        game_id,
+      },
+      token,
+    });
+    const data = await response.getMunzeeData();
+    return [
+      ...new Set(
+        Object.values(data.data?.data.levels ?? {}).flatMap(i => [
+          ...i.individual.map(i => getCuppaZeeTaskId(i)),
+          ...i.group.map(i => getCuppaZeeTaskId(i)),
+        ])
+      ),
+    ];
+  } catch {
+    return null;
+  }
+}, 1000 * 60 * 60 * 24);
+
+// const tasksByGameId: { [game_id: number]: number[] } = {
+//   105: [1, 10, 13, 19, 24, 36, 3, 30, 35, 37, 12, 26, 28, 31],
+//   106: [1, 2, 6, 12, 24, 28, 30, 35, 36, 37, 5038, 4038, 3038, 2038, 3, 13, 14, 26, 34],
+//   107: [1, 2, 6, 13, 19, 24, 35, 10038, 8038, 6038, 5038, 3038, 3, 37, 27, 26, 28, 29, 32, 36],
+//   108: [1, 2, 6, 7, 13, 24, 8038, 7038, 5038, 4038, 3038, 3, 17, 30, 35, 37, 26, 28, 33, 36],
+//   109: [1, 2, 6, 13, 24, 34, 36, 8038, 6038, 4038, 3038, 3, 35, 37, 12, 14, 26, 28, 30, 31],
+//   110: [1, 2, 6, 13, 24, 28, 30, 8038, 6038, 4038, 3038, 3, 27, 35, 37, 7, 19, 26, 29, 36],
+//   111: [1, 2, 6, 13, 17, 24, 31, 8038, 6038, 4038, 3038, 3, 35, 37, 14, 26, 28, 30, 36],
+//   112: [1, 2, 6, 7, 13, 24, 36, 8038, 6038, 4038, 3038, 3, 35, 37, 26, 28, 30, 32, 34],
+//   113: [1, 2, 6, 13, 14, 24, 29, 6038, 5038, 4038, 3038, 2038, 1038, 3, 35, 37, 17, 26, 28, 30, 36],
+// };
 
 export async function getShadowPlayerStats(player: ShadowPlayerReference) {
   const tasksOutput: { [task_id: number]: number | null } = {};
@@ -60,14 +98,16 @@ export async function getShadowPlayerStats(player: ShadowPlayerReference) {
     });
   }
 
-  if (!tasksByGameId[player.game_id]) {
+  const gameIdTasks = await tasksByGameId.get(player.game_id);
+
+  if (!gameIdTasks) {
     console.error(`[IMPORTANT] No tasks for game ${player.game_id}`);
     rollbar?.error(`[IMPORTANT] No tasks for game ${player.game_id}`);
   }
 
   const activityLoader = new ShadowPlayerActivityLoader(player.user_id);
   await Promise.all(
-    tasksByGameId[player.game_id].map(async task_id => {
+    gameIdTasks.map(async task_id => {
       tasksOutput[task_id] = await getShadowPlayerTask(
         {
           ...player,
